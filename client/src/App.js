ececb8bd4fb4cc761793156a0cf440656adee280
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-28 12:23:51 -0700

    add auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,28 @@ import Navbar from "./components/layout/Navbar"
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState"
+import AuthState from "./context/auth/AuthState"
 import './App.css';
+import authContext from './context/auth/authContext';
 
 const App = () => {
 
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <authContext>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </authContext>
   );
 }
 
