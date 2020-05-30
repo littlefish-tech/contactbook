9069bebe95a70127f6ab00b13b68120e85ae8799
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-30 15:18:33 -0700

    update expiration time

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post("/", [
         }
 
         jwt.sign(payload, config.get("jwtSecret"), {
-            expiresIn: 360000
+            expiresIn: 36000
         }, (err, token) => {
             if (err) throw err;
             res.json({ token });
