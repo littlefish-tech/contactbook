b58d5656a6d8a91f3b4eddcc637fb1fd6d0cc0a9
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-30 14:41:57 -0700

    fixed delete card

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useContext, useEffect } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from "./ContactItem";
 import ContactContext from "../../context/contact/contactContext";
 import Spinner from "../../components/layout/Spinner";
@@ -19,14 +19,29 @@ const Contacts = () => {
     }
     return (
         <Fragment>
-            {contacts !== null && !loading ? (<TransitionGroup>
-                {filtered !== null ? filtered.map(contact => (
-                    <CSSTransition key={contact._id} timeout={500} classNames="item">
-                        <ContactItem contact={contact} />
-                    </CSSTransition>
-                )) :
-                    contacts.map(contact => (<ContactItem key={contact._id} timeout={500} contact={contact} />))}
-            </TransitionGroup>) : <Spinner />}
+            {contacts !== null && !loading ? (
+                <TransitionGroup>
+                    {filtered !== null ? filtered.map(contact => (
+                        <CSSTransition key={contact._id}
+                            timeout={500}
+                            classNames="item"
+                        >
+                            <ContactItem contact={contact} />
+                        </CSSTransition>
+                    ))
+                        :
+                        contacts.map(contact => (
+                            <CSSTransition
+                                key={contact._id}
+                                timeout={500}
+                                className="item"
+                            >
+                                <ContactItem contact={contact} />
+                            </CSSTransition>))}
+                </TransitionGroup>
+            ) : (
+                    <Spinner />
+                )}
 
         </Fragment>
     )
